46d6a4d06a387dc2f4742b34f7777a521252e7b1
RexCorpDev <rexcorpfx@gmail.com>
2016-08-26 20:53:05 -0700

    wiring up a add message functionality

--- main.js
+++ main.js
@@ -2,13 +2,12 @@
 'use strict';
 
 
-let rootUrl = 'https://testfirebase-50970.firebaseio.com/',
-    usersRef= new Firebase(rootUrl + 'users'),
-    userObjectsRef = new Firebase(rootUrl + 'userObjects'),
-    timelineRef = new Firebase(userObjectsRef + 'timeline'),
-    userHandler,
-    timelineHandler;
-
+// let rootUrl = 'https://testfirebase-50970.firebaseio.com/',
+//     usersRef= new Firebase(rootUrl + 'users'),
+//     userObjectsRef = new Firebase(rootUrl + 'userObjects'),
+//     timelineRef = new Firebase(userObjectsRef + 'timeline'),
+//     userHandler,
+//     timelineHandler;
 // let fbUsers;
 // usersRef.once('value', (snapshot) => {
 //   console.log('ONCE: ', snapshot.val());
@@ -50,110 +49,126 @@ let rootUrl = 'https://testfirebase-50970.firebaseio.com/',
 // });
 
 
-userObjectsRef.child('following').once('value', (snapshot) => {  // show users in following.
-  let users = snapshot.val();
-
-  for (let userKey in users) {
-    let followerObj = users[userKey]; // an Object of keys that represent followers.
-    console.log('User: ', userKey, '\nfollowers: ', followerObj);
-  }
-});
+// userObjectsRef.child('following').once('value', (snapshot) => {  // show users in following.
+//   let users = snapshot.val();
+//
+//   for (let userKey in users) {
+//     let followerObj = users[userKey]; // an Object of keys that represent followers.
+//     console.log('User: ', userKey, '\nfollowers: ', followerObj);
+//   }
+// });
 
 
 // in order to remember the firebase key of an object, we need to make a new
 // property on that object with key attached. This code below can help and returns
 // an array of objects with that added property.
-let flatten = objects => {
-  // let keys = Object.keys(objects),
-  // i = keys.length,
-  // result = [],
-  // tweet;
-  //
-  // while(i--) {
-  //   tweet = objects[keys[i]];
-  //   tweet.key = keys[i];
-  //   results.push(tweet);
-  // }
-  // return results;
-  let tweet;
-  let results = Object.keys(objects).map((key) => {
-    tweet = objects[key];
-    tweet.key = key;
-    return tweet;
-  });
-  return results
-};
-
-function stopListening() {
-  if (typeof timelineRef === 'object' && typeof timelineHandler) {
-    timelineRef.off('value', timelineHandler);
-  }
-  if (typeof userRef === 'object' && typeof userHandler) {
-    userRef.off('value', userHandler);
-  }  
-}
-
-
-let userChange = (e) => {  // when a different user is selected.
-  let userKey = e.target.value;
-
-  if (userKey) {
-    // gives an object of nested objects, with the first object being a firebase key,
-    // that have a nested tweet and it's details as it's value.
-    let timelineRef = userObjectsRef.child('timeline').child(userKey);
-    // encapsulate the .on() instance so that we turn it off and re-assign it later.
-    timelineHandler = timlineRef.on('value', (snap) => {
-      // this will eventually look like an array of objects, with the firebase key as an added property.
-      let data = flatten(snap.val());
-    });
-
-    let userRef = usersRef.child(userKey);
-    userHandler = userRef.on('value', (snap) => {
-      let fbUser = snap.val();
-    });
-  }
-}
-
-
-
-
-
-// user.on('value', (user_snapshot) => {
-//   console.log('user 1: ', user_snapshot);
-// });
-
-
-// $(document).ready(init);
 //
-// function init() {
-//   $('#add').click(addMessage);
-//   $("#start").click(start);
-//   $("#logout").click(logout);
-//
-//   $(window).on('beforeunload', logout)
-//
-//   chatRef.on('child_added', function(snapshot){
-//     var value = snapshot.val();
-//     var $li = $('<li>').text(value.time + ' ' + value.name + ' ' + value.message);
-//     $('#output').append($li);
+// let flatten = objects => {
+//   // let keys = Object.keys(objects),
+//   // i = keys.length,
+//   // result = [],
+//   // tweet;
+//   //
+//   // while(i--) {
+//   //   tweet = objects[keys[i]];
+//   //   tweet.key = keys[i];
+//   //   results.push(tweet);
+//   // }
+//   // return results;
+//   let tweet;
+//   let results = Object.keys(objects).map((key) => {
+//     tweet = objects[key];
+//     tweet.key = key;
+//     return tweet;
 //   });
-//
-//   usersRef.on('value', function(snapshot){
-//     $('#users').empty();
-//
-//     var users = snapshot.val();
-//
-//     var $lis = [];
-//
-//     for(var key in users){
-//       var $li = $('<li>').text(users[key]);
-//       $lis.push($li);
-//     }
-//     $('#users').append($lis);
-//   });
-//
+//   return results
+// };
+
+// function stopListening() {
+//   if (typeof timelineRef === 'object' && typeof timelineHandler) {
+//     timelineRef.off('value', timelineHandler);
+//   }
+//   if (typeof userRef === 'object' && typeof userHandler) {
+//     userRef.off('value', userHandler);
+//   }
 // }
+
+// let userChange = (e) => {  // when a different user is selected.
+//   let userKey = e.target.value;
 //
+//   if (userKey) {
+//     // gives an object of nested objects, with the first object being a firebase key,
+//     // that have a nested tweet and it's details as it's value.
+//     let timelineRef = userObjectsRef.child('timeline').child(userKey);
+//     // encapsulate the .on() instance so that we turn it off and re-assign it later.
+//     timelineHandler = timlineRef.on('value', (snap) => {
+//       // this will eventually look like an array of objects, with the firebase key as an added property.
+//       let data = flatten(snap.val());
+//     });
+//
+//     let userRef = usersRef.child(userKey);
+//     userHandler = userRef.on('value', (snap) => {
+//       let fbUser = snap.val();
+//     });
+//   }
+// }
+
+
+let rootUrl = 'https://testfirebase-50970.firebaseio.com/',
+    usersRef= new Firebase(rootUrl + 'users'),
+    userObjectsRef = new Firebase(rootUrl + 'userObjects'),
+    timelineRef = new Firebase(userObjectsRef + 'timeline'),
+    userHandler,
+    timelineHandler;
+
+$(document).ready(init);
+
+function init() {
+  $('#add').click(addMessage);
+  $("#start").click(start);
+  $("#logout").click(logout);
+
+  $(window).on('beforeunload', logout)
+
+  // chatRef.on('child_added', function(snapshot){
+  //   var value = snapshot.val();
+  //   var $li = $('<li>').text(value.time + ' ' + value.name + ' ' + value.message);
+  //   $('#output').append($li);
+  // });
+
+  usersRef.on('value', function(snapshot){
+    $('#users').empty();
+
+    var users = snapshot.val();
+
+    var $lis = [];
+
+    for(var key in users){
+      var $li = $('<li>').text(users[key]);
+      $lis.push($li);
+    }
+    $('#users').append($lis);
+  });
+}
+
+function addMessage(e){
+  e.prevent.default();
+  let tweet = {
+    text: $('#message').val(),
+    created: (new Date()).toString()
+  };
+
+  let tweetObjs = userObjectsRef.child('tweets').once('value', (snapshot) => snapshot.val());
+
+  // var message = $("#message").val();
+  // $("#message").val('');
+  // chatRef.push({
+  //   name: name,
+  //   time: Date.now(),
+  //   message: message
+  // });
+}
+
 // function logout(){
 //   usersRef.once('value', function(snapshot){
 //     var users = snapshot.val();
@@ -180,14 +195,4 @@ let userChange = (e) => {  // when a different user is selected.
 //   } else {
 //     alert('Invalid name.');
 //   }
-// }
-//
-// function addMessage(){
-//   var message = $("#message").val();
-//   $("#message").val('');
-//   chatRef.push({
-//     name: name,
-//     time: Date.now(),
-//     message: message
-//   });
 // }
